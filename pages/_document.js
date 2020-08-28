57eab60cba406e136012159470555e5961febed6
cexlt <dev@cexlt.io>
2020-08-28 21:57:39 +0900

    meta tag updated

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,15 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>  
+        <Head>
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Centralized exchange token’s Liquidity Token" />
+          <meta property="og:description" content="Bring decentralization and liquidity to centralized tokens" />
+          <meta property="og:url" content="https://cexlt.io" />
+          <meta property="og:site_name" content="cexlt.io" />
+          <meta property="og:image" content="https://cexlt.io/assets/assets/images/clt-icon.png" />
+          <meta property="og:image:secure_url" content="https://cexlt.io/assets/assets/images/clt-icon.png" />
         </Head>
         <body>
           <Main />
